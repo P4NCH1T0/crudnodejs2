9c20e3916153b2a0ff3bdc544124338c1573f4e3
Fran <98226655+P4NCH1T0@users.noreply.github.com>
2025-04-07 18:34:46 -0600

    Creacion de funciones de la base de datos

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import mysql from "mysql2";
+import cors from "cors"
 
 const  app = express()
+
+// Coneccion con la base de datos
 
 const db = mysql.createConnection({
     host:"localhost",
@@ -11,9 +14,14 @@ const db = mysql.createConnection({
 
 })
 
+app.use(express.json())
+app.use(cors())
+
 app.get("/" ,(req,res)=>{
     res.json("this is the server")
 })
+
+// Mostrar empleados
 
 app.get("/empleado",(req,res)=>{
     const q = "SELECT * from empleado"
@@ -22,6 +30,8 @@ app.get("/empleado",(req,res)=>{
             return res.json(data)
     })
 })
+
+// Crear empleado
 
 app.post("/empleado",(req,res)=>{
     const q = "INSERT INTO empleado (`nombre`,`job`,`shift`,`experience`) VALUES (?)"
@@ -36,6 +46,37 @@ app.post("/empleado",(req,res)=>{
         if(err) return res.json(err);
         return res.json("Employe created sucesfully");
     })
+})
+
+// Borrar empleado
+
+app.delete("/empleado/:id", (req,res)=>{
+    const empleadoId = req.params.id
+    const q = "DELETE FROM empleado WHERE id = ?"
+
+    db.query(q,[empleadoId], (err, data)=>{
+        if(err) return res.json(err)
+            return res.json("empleado has been deleted")
+    });
+})
+
+// Actualizar empleado
+
+app.put("/empleado/:id", (req,res)=>{
+    const empleadoId = req.params.id
+    const q = "UPDATE empleado SET `nombre` = ?, `job` = ?, `shift` =?, `experience` =? WHERE id = ?"
+
+    const values = [
+        req.body.nombre,
+        req.body.job,
+        req.body.shift,
+        req.body.experience
+    ];
+
+    db.query(q,[...values, empleadoId], (err, data)=>{
+        if(err) return res.json(err)
+            return res.json("empleado has been updated")
+    });
 })
 
 app.listen(9900, ()=>{

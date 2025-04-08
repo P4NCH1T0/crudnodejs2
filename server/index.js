fc204114e8d5d074d6707a21222119091dd36d99
Fran <98226655+P4NCH1T0@users.noreply.github.com>
2025-04-07 18:03:57 -0600

    Conexion con la base de datos

--- server/index.js
+++ server/index.js
@@ -1,7 +1,44 @@
 import express from "express";
+import mysql from "mysql2";
 
 const  app = express()
 
-app.listen(8800, ()=>{
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"fran2305",
+    database:"crud2"
+
+})
+
+app.get("/" ,(req,res)=>{
+    res.json("this is the server")
+})
+
+app.get("/empleado",(req,res)=>{
+    const q = "SELECT * from empleado"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+            return res.json(data)
+    })
+})
+
+app.post("/empleado",(req,res)=>{
+    const q = "INSERT INTO empleado (`nombre`,`job`,`shift`,`experience`) VALUES (?)"
+    const values =[
+        "Leo",
+        "3D Modeler",
+        "Matutino",
+        "2"
+    ];
+
+    db.query(q,[values], (err,data)=>{
+        if(err) return res.json(err);
+        return res.json("Employe created sucesfully");
+    })
+})
+
+app.listen(9900, ()=>{
     console.log("Conected to the server")
 })
+
